41d805072ba3d191a1f3a40e26e9710c5dc876f5
Stanislaw Klekot <dozzie@jarowit.net>
2016-10-24 21:43:06 +0200

    Added function for replaying events from log file to statip_flog module.

--- src/statip_flog.erl
+++ src/statip_flog.erl
@@ -11,12 +11,15 @@
 
 %% public interface
 -export([open/2, close/1]).
--export([append/5, read/2, recover/2]).
+-export([append/5, read/2, recover/2, file_size/1]).
+-export([replay/3, replay/4, fold/3]).
 -export([format_error/1]).
 
 -export_type([handle/0, entry/0]).
+-export_type([records/0, mapping/2, single_values/0, burst_values/0]).
 
 -on_load(load_required_atoms/0).
+-export([load_required_atoms/0]).
 
 %%%---------------------------------------------------------------------------
 %%% types {{{
@@ -25,12 +28,17 @@
 
 -type handle() :: {flog_write, file:io_device()}
                 | {flog_read, file:io_device()}.
+%% Log file handle.
 
 -type entry() ::
     {clear,  statip_value:name(), statip_value:origin()}
   | {clear,  statip_value:name(), statip_value:origin(), statip_value:key()}
   | {single, statip_value:name(), statip_value:origin(), #value{}}
   | {burst,  statip_value:name(), statip_value:origin(), #value{}}.
+%% Record entry read from log file.
+
+%%----------------------------------------------------------
+%% record header on-disk data {{{
 
 %% XXX: magic should be a sequence of unique bytes
 -define(RECORD_HEADER_MAGIC, <<166,154,184,182,146,161,251,150>>).
@@ -42,6 +50,33 @@
 -define(TYPE_CLEAR_KEY, <<"k">>).
 -define(TYPE_SINGLE,    <<"s">>).
 -define(TYPE_BURST,     <<"b">>).
+
+%% }}}
+%%----------------------------------------------------------
+%% replay()/fold() types {{{
+
+-type records() ::
+  mapping({statip_value:name(), statip_value:origin()},
+          single_values() | burst_values()).
+%% A container with records replayed from disk. See {@link replay/3} and
+%% {@link fold/3}.
+
+%% @type mapping(Key, Value) = gb_tree().
+%%   Mapping with keys being of `Key' type and values being of `Value' type.
+
+-type mapping(_Key, _Value) :: gb_tree().
+
+-type single_values() :: {single, mapping(statip_value:key(), #value{})}.
+%% Sub-container for {@type records()}, used for storing single (non-burst)
+%% values.
+
+-type burst_values() ::
+  {burst, Current :: mapping(statip_value:key(), #value{}) | none,
+          Old     :: mapping(statip_value:key(), #value{}) | none}.
+%% Sub-container for {@type records()}, used for storing burst values.
+
+%% }}}
+%%----------------------------------------------------------
 
 %%% }}}
 %%%---------------------------------------------------------------------------
@@ -79,10 +114,30 @@ close({flog_write, FH} = _Handle) ->
   file:close(FH),
   ok.
 
+%% @doc Read size of an opened log file.
+
+-spec file_size(handle()) ->
+  {ok, non_neg_integer()} | {error, file:posix() | badarg}.
+
+file_size({flog_read, FH} = _Handle) ->
+  case file:position(FH, cur) of
+    {ok, OldPosition} ->
+      {ok, EOFPos} = file:position(FH, eof),
+      {ok, _} = file:position(FH, OldPosition),
+      {ok, EOFPos};
+    {error, Reason} ->
+      {error, Reason}
+  end;
+file_size({flog_write, FH} = _Handle) ->
+  case file:position(FH, cur) of
+    {ok, Position} -> {ok, Position};
+    {error, Reason} -> {error, Reason}
+  end.
+
 %% @doc Append an entry to a log file opened for writing.
 
--spec append(handle(), statip_value:name(), statip_value:origin(),
-             Entry | [Entry], single | burst) ->
+-spec append(handle(), statip_value:name(), statip_value:origin(), Entry,
+             single | burst) ->
   ok | {error, read_only | file:posix() | badarg}
   when Entry :: #value{} | clear | {clear, statip_value:key()}.
 
@@ -192,6 +247,90 @@ recover({flog_read, FH} = _Handle, ReadBlock) ->
     {error, Reason} ->
       {error, Reason}
   end.
+
+%% @doc Replay events from a log file.
+%%
+%%   Function returns a sequence of records that represent what should be
+%%   present state of remembered values, according to the log.
+%%
+%%   The log file should start with a valid record, otherwise
+%%   `{error,bad_file}' is returned.
+%%
+%%   Some of the records in the log file can be damaged, in which case
+%%   automatic recovery is performed. If no valid record could be found for
+%%   `RecoverTries' blocks of `ReadBlock' bytes each, the file is deemed
+%%   to be damaged and `{error,damaged_file}' is returned.
+%%
+%% @see replay/4
+%% @see fold/3
+
+-spec replay(handle(), pos_integer(), pos_integer()) ->
+  {ok, records()} | {error, Reason}
+  when Reason :: write_only | bad_file | damaged_file | file:posix() | badarg.
+
+replay(Handle, ReadBlock, RecoverTries) ->
+  State = gb_trees:empty(),
+  case read(Handle, ReadBlock) of
+    {ok, Entry} ->
+      NewState = replay_add_entry(Entry, State),
+      replay(Handle, ReadBlock, RecoverTries, NewState);
+    eof ->
+      {ok, State};
+    {error, bad_record} ->
+      {error, bad_file};
+    {error, Reason} ->
+      {error, Reason}
+  end.
+
+%% @doc Continue replaying events from a log file.
+%%
+%% @see replay/3
+%% @see fold/3
+
+-spec replay(handle(), pos_integer(), pos_integer(), records()) ->
+  {ok, records()} | {error, Reason}
+  when Reason :: damaged_file | file:posix() | badarg.
+
+replay(Handle, ReadBlock, RecoverTries, State) ->
+  case read(Handle, ReadBlock) of
+    {ok, Entry} ->
+      NewState = replay_add_entry(Entry, State),
+      replay(Handle, ReadBlock, RecoverTries, NewState);
+    eof ->
+      {ok, State};
+    {error, bad_record} ->
+      case try_recover(Handle, ReadBlock, RecoverTries) of
+        {ok, Entry} ->
+          NewState = replay_add_entry(Entry, State),
+          replay(Handle, ReadBlock, RecoverTries, NewState);
+        eof ->
+          {ok, State};
+        none ->
+          {error, damaged_file};
+        {error, Reason} ->
+          {error, Reason}
+      end;
+    {error, Reason} ->
+      {error, Reason}
+  end.
+
+%% @doc Fold over records replayed from a log file.
+%%
+%%   Note that unlike {@link lists:foldl/3}, `Fun' is called with a <em>list
+%%   of records</em> that have the same value name and value origin.
+%%
+%% @see replay/3
+%% @see replay/4
+
+-spec fold(Fun, AccIn :: term(), records()) ->
+  AccOut :: term()
+  when Fun :: fun((Key, Type, Records, Acc :: term()) -> NewAcc :: term()),
+       Key :: {statip_value:name(), statip_value:origin()},
+       Type :: single | burst,
+       Records :: [#value{}].
+
+fold(Fun, AccIn, Records) ->
+  foreach(Fun, AccIn, gb_trees:iterator(Records)).
 
 %% @doc Describe an error returned by any function from this module.
 
@@ -510,7 +649,138 @@ null_undefined(Bin) -> Bin.
 %%----------------------------------------------------------
 
 %%%---------------------------------------------------------------------------
+%%% replay events from file
 
+%%----------------------------------------------------------
+%% try_recover() {{{
+
+%% @doc Try recovering a valid record for specified number of blocks.
+%%
+%% @see recover/2
+
+-spec try_recover(handle(), pos_integer(), pos_integer()) ->
+  {ok, entry()} | none | eof | {error, file:posix() | badarg}.
+
+try_recover(_Handle, _ReadBlock, 0 = _RecoverTries) ->
+  none;
+try_recover(Handle, ReadBlock, RecoverTries) when RecoverTries > 0 ->
+  case recover(Handle, ReadBlock) of
+    {ok, Entry} -> {ok, Entry};
+    eof -> eof;
+    none -> try_recover(Handle, ReadBlock, RecoverTries - 1);
+    {error, Reason} -> {error, Reason}
+  end.
+
+%% }}}
+%%----------------------------------------------------------
+%% replay_add_entry() {{{
+
+%% @doc Update a sequence of values with a new log entry.
+
+-spec replay_add_entry(entry(), records()) ->
+  records().
+
+replay_add_entry({clear, Name, Origin} = _Entry, State) ->
+  case gb_trees:lookup({Name, Origin}, State) of
+    {value, {single, _KeyMap}} ->
+      _NewState = gb_trees:delete({Name, Origin}, State);
+    {value, {burst, none = _KeyMap, _OldKeyMap}} ->
+      _NewState = gb_trees:delete({Name, Origin}, State);
+    {value, {burst, KeyMap, _OldKeyMap}} ->
+      _NewState = gb_trees:enter({Name, Origin}, {burst, none, KeyMap}, State);
+    none ->
+      State
+  end;
+
+replay_add_entry({clear, Name, Origin, Key} = _Entry, State) ->
+  case gb_trees:lookup({Name, Origin}, State) of
+    {value, {single, _KeyMap}} ->
+      NewValue = gb_trees:delete_any(Key, State),
+      _NewState = gb_trees:enter({Name, Origin}, NewValue, State);
+    {value, {burst, _KeyMap, _OldKeyMap}} ->
+      State; % operation not expected for burst values
+    none ->
+      State
+  end;
+
+replay_add_entry({single, Name, Origin, Value = #value{key = Key}} = _Entry,
+                 State) ->
+  case gb_trees:lookup({Name, Origin}, State) of
+    {value, {single, KeyMap}} ->
+      NewKeyMap = gb_trees:enter(Key, Value, KeyMap),
+      _NewState = gb_trees:enter({Name, Origin}, {single, NewKeyMap}, State);
+    {value, {burst, _KeyMap, _OldKeyMap}} ->
+      State; % record incompatible with remembered value type
+    none ->
+      NewKeyMap = gb_trees:empty(),
+      _NewState = gb_trees:enter({Name, Origin}, {single, NewKeyMap}, State)
+  end;
+
+replay_add_entry({burst, Name, Origin, Value = #value{key = Key}} = _Entry,
+                 State) ->
+  case gb_trees:lookup({Name, Origin}, State) of
+    {value, {single, _KeyMap}} ->
+      State; % record incompatible with remembered value type
+    {value, {burst, KeyMap, OldKeyMap}} ->
+      NewKeyMap = case KeyMap of
+        none -> gb_trees:insert(Key, Value, gb_trees:empty());
+        _    -> gb_trees:enter(Key, Value, KeyMap)
+      end,
+      NewValue = {burst, NewKeyMap, OldKeyMap},
+      _NewState = gb_trees:enter({Name, Origin}, NewValue, State);
+    none ->
+      NewKeyMap = gb_trees:insert(Key, Value, gb_trees:empty()),
+      NewValue = {burst, NewKeyMap, none},
+      _NewState = gb_trees:enter({Name, Origin}, NewValue, State)
+  end.
+
+%% }}}
+%%----------------------------------------------------------
+
+%%%---------------------------------------------------------------------------
+%%% fold() helpers
+
+%%----------------------------------------------------------
+%% foreach() {{{
+
+%% @doc Iterate through {@type gb_tree} for {@link fold/3}.
+
+-spec foreach(fun(), term(), gb_trees:iter()) ->
+  term().
+
+foreach(Fun, Acc, Iterator) ->
+  case gb_trees:next(Iterator) of
+    {Key, {single, RecTree}, NewIterator} ->
+      Records = gb_trees:values(RecTree),
+      NewAcc = Fun(Key, single, Records, Acc),
+      foreach(Fun, NewAcc, NewIterator);
+    {Key, {burst, RecTree, OldRecTree}, NewIterator} ->
+      Records = burst_records(RecTree, OldRecTree),
+      NewAcc = Fun(Key, burst, Records, Acc),
+      foreach(Fun, NewAcc, NewIterator);
+    none ->
+      Acc
+  end.
+
+%burst_records(none = _RecTree, none = _OldRecTree) ->
+%  []; % should never happen; `replay_add_entry()' guarantees that
+burst_records(none = _RecTree, OldRecTree) ->
+  gb_trees:values(OldRecTree);
+burst_records(RecTree, none = _OldRecTree) ->
+  gb_trees:values(RecTree);
+burst_records(RecTree, OldRecTree) ->
+  _Records = gb_trees:values(RecTree) ++ [
+    V ||
+    V = #value{key = Key} <- gb_trees:values(OldRecTree),
+    not gb_trees:is_defined(Key, RecTree)
+  ].
+
+%% }}}
+%%----------------------------------------------------------
+
+%%%---------------------------------------------------------------------------
+
+%% @private
 %% @doc Load all atoms that can be stored in records in log file.
 %%
 %%   This prevents {@link read/2} from failing mysteriously on an otherwise
